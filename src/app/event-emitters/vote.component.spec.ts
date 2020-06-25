ed2db78a4f371928ac931f881cdb04998d21c316
nenadnesta <nenadnesta@gmail.com>
2020-06-25 12:36:06 +0200

    Event Emitters.

---
+++ src/app/event-emitters/vote.component.spec.ts
@@ -0,0 +1,22 @@
+import { VoteComponent } from './vote.component';
+
+describe('VoteComponent', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+
+  it('should raise voteChanged event when upvoted', () => {
+    let totalVotes = null;
+    component.voteChanged.subscribe(tv => totalVotes = tv);
+
+    component.upVote();
+
+    // too generic
+    // expect(totalVotes).not.toBeNull();
+
+    expect(totalVotes).toBe(1);
+  });
+});
+
