388360992b05b7426860198cf38c351e348369e0
nenadnesta <nenadnesta@gmail.com>
2020-06-29 14:55:42 +0200

    ITesting - Property and Class Bindings.

--- src/app/it-voter/it-voter.component.spec.ts
+++ src/app/it-voter/it-voter.component.spec.ts
@@ -1,44 +1,52 @@
 import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ItVoterComponent } from './it-voter.component';
 
 describe('ItVoterComponent', () => {
   let fixture: ComponentFixture<ItVoterComponent>;
   let component: ItVoterComponent;
 
-  // Angular CLI generated code
-  // the component template is in the separate file and we need to instruct angular to compile thet template,
-  // as well as the stylesheet with the component implementation
-  // not necessary (when we use webpack it automatically inlines component template and stylesheet in our js bundle)
-
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ ItVoterComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
-
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(ItVoterComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItVoterComponent ]
+      declarations: [ItVoterComponent]
     });
 
     // fixture = wrapper around the component
     fixture = TestBed.createComponent(ItVoterComponent);
     component = fixture.componentInstance;
-
-    // nat Element = returns an html element which is the root dom element for component template
-    let natElement = fixture.nativeElement;
-    // wrapper around nativeElement
-    let dbgElement = fixture.debugElement;
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    // Outside this testing environment Angular regularly runs its change detection algorithm.
+    // Here we have to explicitly tell Angular when to perform change detection.
+    fixture.detectChanges();
+
+    // with this function we want to traverse the DOM and find the first element that matches the given criteria.
+    // queryAll - return all the elements that match the predicate
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+
+    // this above could be also be done with By.directive
+    // - if we have a custom directive and we want to find an element
+    // that has that directive applied to it we can pass the directive type here.
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    // de.classes
+    // de.attributes
+    // de.styles
+
+    const theclass = 'highlighted';
+    expect(de.classes[theclass]).toBeTruthy();
   });
 });
 
