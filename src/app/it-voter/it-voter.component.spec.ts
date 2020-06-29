da60f4e6b66449f69876428c19dbea90ee17ad1b
nenadnesta <nenadnesta@gmail.com>
2020-06-29 12:44:19 +0200

    Integration Testing - Generating the Setup Code.

--- src/app/it-voter/it-voter.component.spec.ts
+++ src/app/it-voter/it-voter.component.spec.ts
@@ -1,9 +1,27 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { ItVoterComponent } from './it-voter.component';
 
 describe('ItVoterComponent', () => {
   let fixture: ComponentFixture<ItVoterComponent>;
   let component: ItVoterComponent;
+
+  // Angular CLI generated code
+  // the component template is in the separate file and we need to instruct angular to compile thet template,
+  // as well as the stylesheet with the component implementation
+  // not necessary (when we use webpack it automatically inlines component template and stylesheet in our js bundle)
+
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ ItVoterComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
+
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(ItVoterComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
